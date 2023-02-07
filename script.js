40fcd73abbfc29ecf3c3740db0ad502aa0671d48
jdrcabrera <jd.rcabrera@gmail.com>
2023-02-06 23:38:37 -0500

    Adding Landingpage project

---
+++ script.js
@@ -0,0 +1,7 @@
+const toggle = document.querySelector(".toggle")
+const navigation = document.querySelector(".navigation")
+
+toggle.onclick=function(){
+    toggle.classList.toggle('active')
+    navigation.classList.toggle('active')
+}
